7c0d3f012489b92de8dd663a2800fb5109a5b4e9
mohamed-Elhossin <m.m.m.elhossin@gmail.com>
2022-11-21 18:28:25 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { WhyUsComponent } from './main/why-us/why-us.component';
+import { TeamComponent } from './main/team/team.component';
+import { PortfolioComponent } from './main/portfolio/portfolio.component';
+import { ServicesComponent } from './main/services/services.component';
+import { AboutComponent } from './main/about/about.component';
+import { HeroComponent } from './hero/hero.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HeroComponent },
+  { path: "", component: HeroComponent },
+  { path: "about", component: AboutComponent },
+  { path: "services", component: ServicesComponent },
+  { path: "portfolio", component: PortfolioComponent },
+  { path: "team", component: TeamComponent },
+  { path: "why-us", component: WhyUsComponent },
+  { path: "404", component: NotFoundComponent },
+  { path: "**", component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './main/about/about.component';
+import { ServicesComponent } from './main/services/services.component';
+import { WhyUsComponent } from './main/why-us/why-us.component';
+import { PortfolioComponent } from './main/portfolio/portfolio.component';
+import { TeamComponent } from './main/team/team.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroComponent,
+    AboutComponent,
+    ServicesComponent,
+    WhyUsComponent,
+    PortfolioComponent,
+    TeamComponent,
+    NotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
